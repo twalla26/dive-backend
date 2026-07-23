dd783eb8427236e6a01298818edddd694dadf327
twalla26 <ssm4748@naver.com>
2026-07-23 20:25:09 +0900

    feat: 프로젝트 초기화

---
+++ src/main/kotlin/com/twalla/divebackend/DiveBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.twalla.divebackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DiveBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<DiveBackendApplication>(*args)
+}
