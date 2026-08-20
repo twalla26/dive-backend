ec8e83f0afc92faa696dc42d881e8fd639366167
twalla26 <ssm4748@naver.com>
2026-08-20 11:41:46 +0900

    feat: comment 엔티티 구현

---
+++ src/main/kotlin/com/twalla/divebackend/comment/Comment.kt
@@ -0,0 +1,43 @@
+package com.twalla.divebackend.comment
+
+import com.twalla.divebackend.post.Post
+import com.twalla.divebackend.user.User
+import jakarta.persistence.*
+import org.hibernate.annotations.ColumnDefault
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.time.Instant
+
+@Entity
+@Table(name = "comment", schema = "dive")
+@EntityListeners(AuditingEntityListener::class)
+class Comment(
+    @Lob
+    @Column(name = "content", nullable = false, length = 2000)
+    val content: String,
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "post_id", nullable = false)
+    val post: Post,
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    val user: User,
+) {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    var id: Long = 0L
+        protected set
+
+    @ColumnDefault("0")
+    @Column(name = "like_count", nullable = false)
+    var likeCount: Int = 0
+        protected set
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false)
+    var createdAt: Instant = Instant.now()
+        protected set
+
+}
